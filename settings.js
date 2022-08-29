06ee52b0d8e7e2261cadd0574c17fffc8891fcdd
Fares1115 <111462638+Fares1115@users.noreply.github.com>
2022-08-30 01:46:36 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['01556086022']
+global.premium = ['01556086022']
+global.ownernomer = '01556086022'
+global.ownername = 'OKABY'
+global.botname = 'ONEPIECE-3MAK'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'OKABY🖇️🖤'
+global.author = 'OKABY 🖇️🖤'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
